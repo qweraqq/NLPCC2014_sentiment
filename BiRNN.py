bfdac87bed2ba8678b1309bcfa8ea5c44074aa00
qweraqq <shenxiangxiang@gmail.com>
2016-04-02 22:41:48 +0800

    update BiRNN

--- BiRNN.py
+++ BiRNN.py
@@ -5,15 +5,16 @@ import numpy as np
 import keras
 from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Activation
 from keras.layers.embeddings import Embedding
-from keras.layers.recurrent import LSTM, GRU
+from keras.layers import LSTM, Dense, Dropout
 from seya.layers.recurrent import Bidirectional
+from keras.regularizers import l2
 
 max_features = 15000
 maxlen = 35  # cut texts after this number of words (among top max_features most common words)
 batch_size = 32
-
+nb_hidden = 200
 print("Loading data...")
 f1 = open('training.set.txt', 'r')
 f2 = open('labels.txt', 'r')
@@ -75,25 +76,31 @@ print(len(X_test), 'test sequences')
 print("Pad sequences (samples x time)")
 print('X_train shape:', X_training.shape)
 print('X_test shape:', X_test.shape)
-#
-lstm = LSTM(output_dim=64)
-gru = GRU(output_dim=64)  # original examples was 128, we divide by 2 because results will be concatenated
-brnn = Bidirectional(forward=lstm, backward=gru)
+
+
+lstm1 = LSTM(output_dim=nb_hidden/2, input_dim=nb_hidden,
+             dropout_U=0.3, dropout_W=0.3, W_regularizer=l2(0.001), b_regularizer=l2(0.001))
+lstm2 = LSTM(output_dim=nb_hidden/2, input_dim=nb_hidden,
+             dropout_U=0.3, dropout_W=0.3, W_regularizer=l2(0.001), b_regularizer=l2(0.001))
+# lstm = LSTM(output_dim=64)
+# gru = LSTM(output_dim=64)
+# original examples was 128, we divide by 2 because results will be concatenated
+brnn = Bidirectional(forward=lstm1, backward=lstm2)
 
 print('Build model...')
 model = Sequential()
-model.add(Embedding(max_features, 128, input_length=maxlen))
+model.add(Embedding(max_features, nb_hidden, input_length=maxlen))
 model.add(brnn)  # try using another Bidirectional RNN inside the Bidirectional RNN. Inception meets callback hell.
-model.add(Dropout(0.8))
+model.add(Dropout(0.6))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
 # try using different optimizers and different optimizer configs
-model.compile(loss='binary_crossentropy', optimizer='RMSprop', class_mode="binary")
+model.compile(loss='binary_crossentropy', optimizer='RMSprop')
 earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss',
                                               patience=0, verbose=0, mode='auto')
 print("Train...")
-model.fit(X_training, y_training, batch_size=batch_size, nb_epoch=5, validation_data=(X_test, y_test),
+model.fit(X_training, y_training, batch_size=batch_size, nb_epoch=10, validation_data=(X_test, y_test),
           show_accuracy=True, shuffle=True, callbacks=[earlyStopping])
 score, acc = model.evaluate(X_test, y_test, batch_size=batch_size, show_accuracy=True)
 y_predict = model.predict(X_test, batch_size=1)
